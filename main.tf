66e8949d7117fedda312b3845816fa0bb66dbd0c
Mrinal Virnave <mrinal.virnave@blackboard.com>
2018-10-03 20:46:16 -0700

    updated main.tf with local provider

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-resource "null_resource" "example1" {
-  provisioner "local-exec" {
-    command = "echo $FOO $BAR $BAZ >> /ansible/playbook/success.txt"
-
-    environment {
-      FOO = "Hello"
-      BAR = "..."
-      BAZ = "World"
-    }
-  }
+resource "local_file" "foo" {
+    content     = "Hello World!"
+    filename = "/ansible/playbook/success.txt"
 }
+
